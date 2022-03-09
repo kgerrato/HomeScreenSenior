5e4eb19501df698f4d58a282f27064d846b03e8c
Konstantino Gerrato <kgerrato1@gmail.com>
2022-03-08 19:49:49 -0500

    Initial Commit

---
+++ app/src/main/java/com/zybooks/homescreensenior/Crypto.java
@@ -0,0 +1,14 @@
+package com.zybooks.homescreensenior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Crypto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto);
+    }
+}
